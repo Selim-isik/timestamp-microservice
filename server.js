d547bc530cbe3c8c8d4d70a35ecc3a2f36f9e10e
Selim Işık <selimoyunda6@gmail.com>
2025-12-12 17:13:22 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+app.get("/api/:date", (req, res) => {
+  let dateString = req.params.date;
+  let date;
+
+  if (/\d{5,}/.test(dateString)) {
+    date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Sunucu şurada çalışıyor: http://localhost:" + port);
+});
